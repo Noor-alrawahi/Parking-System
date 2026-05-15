359b9d8663519b56a69d561cad9edd8a7b958c0a
Mazayin <24-0572@student.gutech.edu.om>
2026-05-15 23:50:19 +0400

    Update payment.cpp

--- payment.cpp
+++ payment.cpp
@@ -1,1 +1,23 @@
 // payment class
+#include "Payment.h"
+
+Payment::Payment() {
+   amount=0;
+}
+
+void Payment::calculate( double rate, int duration, bool wrongSlot, bool disabledViolation, bool disabledPermit){
+   if (disabledPermit){
+     amount=0
+     return ;
+   }
+   amount= rate* duration;
+
+   if (wrongSlot)
+     amount+=5;
+
+   if (disabledViolation)
+     amount+=50;
+
+void Payment::display() const {
+  cout<<"Total payment: "<< amount<<" OMR\n";
+}
