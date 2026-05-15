3cd0c749069d363c792a96fd68ffe25791dc1a37
Mazayin <24-0572@student.gutech.edu.om>
2026-05-15 23:31:55 +0400

    Update parkingSession.cpp

--- parkingSession.cpp
+++ parkingSession.cpp
@@ -1,1 +1,37 @@
 // parkingSession class
+
+#include "ParkingSession.h"
+ParkingSession::ParkingSession(){
+
+   reservationID=0;
+   startHour=0;
+   endHour=0;
+   occupied=false;
+}
+
+void ParkingSession::startSession( int resID, int start){
+    reservationID=resID;
+    startHour=start;
+    occupied=true;
+}
+
+void ParkingSession::endSession(int end){
+  endHour=end;
+  occupied=false;
+}
+
+int ParkingSession::getReservationID() const{
+  return reservationID;
+}
+
+bool ParkingSession::isOccupied() const{
+  return occupied;
+}
+
+int ParkingSession::getDuration() const {
+  int duration=endHour-startHour;
+
+  if (duration<=0)
+    duration+=24;
+  return duration;
+}
