635ea446fe6e905e2bebc1afa22a30e449cb6d97
Mazayin <24-0572@student.gutech.edu.om>
2026-05-15 23:42:25 +0400

    Update payment.h

--- payment.h
+++ payment.h
@@ -1,1 +1,17 @@
 // payment class
+#ifndef PAYMENT_H
+#define PAYMENT_H
+
+#include <iostream>
+using namespace std;
+class Payment{
+private:
+  double amount;
+public:
+  Payment();
+
+  void calculate( double rate, int duration, bool wrongSlot, bool disabledviolation, bool disabledPermit);
+  void display() const;
+};
+
+#endif
