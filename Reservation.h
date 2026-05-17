cac0d605e0ba35cc2b52290ff5934536b159cee1
salbusaidi <24-0143@student.gutech.edu.om>
2026-05-17 19:59:03 +0400

    Update Reservation.h

--- Reservation.h
+++ Reservation.h
@@ -1,1 +1,37 @@
+#ifndef RESERVATION_H
+#define RESERVATION_H
+
+class Reservation {
+private:
+int reservationID;
+int customerID;
+int reservedSlot;
+int actualSlot;
+bool wrongSlot;
+bool active;
+
+public:
+Reservation();
+
+Reservation(
+int resID,
+int custID,
+int slot
+);
+
+int getReservationID() const;
+int getCustomerID() const;
+int getReservedSlot() const;
+int getActualSlot() const;
+
+bool hasWrongSlot() const;
+bool isActive() const;
+
+void setActualSlot(int slot);
+
+void closeReservation();
+};
+
+#endif
+
 
