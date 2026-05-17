67aebd3dd57442110e7b4227d24b3695238d2d52
Noor-alrawahi <24-0680@student.gutech.edu.om>
2026-05-18 02:03:12 +0400

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,1 +1,29 @@
 // Customwe class
+
+#include"Customer.h"
+
+int Customer :: totalCustomers = 0;
+Customer:: () {
+customerID = 0;
+name="";
+phone="";
+plateNumber="";
+disabledPermit= false;
+}
+bool Customer :: isDigitsOnly(string s){
+  if(s=="")
+    return false;
+for(char c : s){
+if(!isDigitChar(c))
+  return false;
+}
+return true;
+}
+int Customer :: stringToInt(string s){
+  int number =0;
+for(char c : s )
+  number=number*10+(c-'0');
+return number;
+}
+    
+
