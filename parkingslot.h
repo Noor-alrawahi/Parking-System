ff2aa1f51dbb6b0024a29a3b5e9adf365bcbed02
rayyanalabdusalam <24-0413@student.gutech.edu.om>
2026-05-18 00:43:54 +0400

    Update parkingslot.h

--- parkingslot.h
+++ parkingslot.h
@@ -1,1 +1,90 @@
+#idndef PARKINGSLOT_H
+#define PARKINGSLOT_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ParkingSlot{
+
+protected: //derived class can access this 
+int slotNumber;
+bool occupied;
+bool reserved;
+bool disabled;
+
+public:
+ParkingSlot(int num, disabled); //constructor
+virtual ~ParkingSlot(); //distructor
+
+int getSlotNumber() const;
+bool isOccupied() const;
+bool isReserved() const;
+bool isDisabled() const;
+
+void reserve();
+void occupy();
+void freeSlot();
+
+virtual string getType() const = 0; //pure virtual
+virtual double getRate() const = 0; //pure virtual
+virtual void display() const;
+
+bool operator==(const ParkingSlot& other);
+
+}; //base class ends
+
+class SunnySlot final : public ParkingSlot{ 
+
+public:  
+SunnySlot(int num, bool disabled);
+
+string getType() const override;
+double getRate() const override;
+
+};
+
+class ShaddedSlot final : public ParkingSlot{
+
+public:
+ShaddedSlot(int num, bool disabled);
+
+string getType() const override;
+double getRate() const override;
+
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
