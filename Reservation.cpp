74580f3adff14b4928238e9b3b67a6242db5fad1
salbusaidi <24-0143@student.gutech.edu.om>
2026-05-17 20:08:52 +0400

    Update Reservation.cpp

--- Reservation.cpp
+++ Reservation.cpp
@@ -1,1 +1,61 @@
+#include "Reservation.h"
+
+Reservation::Reservation() {
+
+reservationID = 0;
+customerID = 0;
+reservedSlot = 0;
+actualSlot = 0;
+wrongSlot = false;
+active = false;
+}
+
+Reservation::Reservation(
+int resID,
+int custID,
+int slot
+) {
+
+reservationID = resID;
+customerID = custID;
+reservedSlot = slot;
+actualSlot = 0;
+wrongSlot = false;
+active = true;
+}
+
+int Reservation::getReservationID() const {
+  return reservationID;
+}
+
+int Reservation::getCustomerID() const {
+  return customerID;
+}
+
+int Reservation::getActualSlot() const {
+  return actualSlot;
+}
+
+bool Reservation::hasWrongSlot() const {
+  return wrongSlot;
+}
+
+bool Reservation::isActive() const {
+  return active;
+}
+
+void Reservation::setActualSlot(int slot) {
+
+  actualSlot = slot;
+
+if (actualSlot != reservedSlot)
+  wrongSlot = true;
+
+}
+
+void Reservation::closeReservation() {
+  active = false;
+}
+
+
 
