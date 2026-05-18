16bdd76e40ff7b266aca2a1025401c95443406c8
Noor-alrawahi <24-0680@student.gutech.edu.om>
2026-05-18 15:17:33 +0400

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -15,33 +15,33 @@ string phone;
 string plateNumber;
 bool disabledPermit;
 static int totalCustomers;
-static bool is DigitChar(char c);
-static bool is LetterChar(char c);
-static bool is DigitsOnly(string s);
+static bool isDigitChar(char c);
+static bool isLetterChar(char c);
+static bool isDigitsOnly(string s);
 static int stringToInt(string s);
 static string toUpperText(string s);
 
-Public:
+public:
 
 Customer();
 static int readInt(string message, int minValue, int maxValue);
 static int readExactDigitsInt( string message, int digits);
 static string readName();
-static striing readPhone();
+static string readPhone();
 static string readPlateNumber();
 static bool readYesNo(string message);
-static bool is ValidPermit(string permit);
+static bool isValidPermit(string permit);
 static bool readDisabledPermit();
 static int convertTo24Hour( int hour, string ampm);
 static int readHourWithAmPm(string message);
 
 void setData( int d, string n, string ph, string plate, bool permit);
-int getID()const;
-bool hasDisabledePermit()const;
+int getID() const;
+bool hasDisabledPermit() const;
 static int getTotalCustomers();
-bool operator==(const Customer & other)const;
+bool operator==(const Customer & other) const;
 
-friend void displayCustomer( const Customer & c);
+friend void displayCustomer(const Customer & c);
 };
 
 #endif
