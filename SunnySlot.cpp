8265501d2ae32c246acaae9acffeecad681076aa
rayyanalabdusalam <24-0413@student.gutech.edu.om>
2026-05-21 03:27:33 +0400

    Update SunnySlot.cpp

--- SunnySlot.cpp
+++ SunnySlot.cpp
@@ -1,1 +1,12 @@
+#include "SunnySlot.h"
+
+SunnySlot::SunnySlot(int num, bool disabled): ParkingSlot(num, disabled) {}
+
+string SunnySlot::getType() const{
+  return "Sunny Slot";
+}
+
+double SunnySlot::getRate() const {
+  return 1;
+}
 
