75d14a3adc38a38218d349dc8f770873bc74a92f
rayyanalabdusalam <24-0413@student.gutech.edu.om>
2026-05-21 03:31:41 +0400

    Update ShaddedSlot.h

--- ShaddedSlot.h
+++ ShaddedSlot.h
@@ -1,1 +1,13 @@
+#ifndef SHADDEDSLOT_H
+#define SHADDEDSLOT_H
+#include "ParkingSlot.h"
 
+class ShaddedSlot final : public ParkingSlot{
+public:
+ShaddedSlot(int num,bool disabled);
+
+string getType() const override;
+double getRate() const override;
+};
+
+#endif
