cb01beb4b8afe6a4401bfb0e77da3eac32587b8c
rayyanalabdusalam <24-0413@student.gutech.edu.om>
2026-05-21 03:35:45 +0400

    Update ShaddedSlot.cpp

--- ShaddedSlot.cpp
+++ ShaddedSlot.cpp
@@ -1,1 +1,11 @@
+#include "ShaddedSlot.h"
 
+ShaddedSlot::ShaddedSlot(int num, bool disabled):ParkingSlot(num, disabled) {}
+
+string ShaddedSlot::getType() const{
+  return "Shadded Slot";
+}
+
+double ShaddedSlot::getRate() const{
+  return 2;
+}
