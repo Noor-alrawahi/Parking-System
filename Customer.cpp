6f88e7bd6193be1f9795e0ab0d925f1f34ccab9a
Noor-alrawahi <24-0680@student.gutech.edu.om>
2026-05-21 23:42:51 +0400

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -3,13 +3,7 @@
 #include "Customer.h"
 
 int Customer::totalCustomers = 0;
-Customer::Customer() {
-customerID = 0;
-name="";
-phone="";
-plateNumber="";
-disabledPermit= false;
-}
+
 bool Customer::isDigitChar(char c) {
 return c >='0' && c<='9';
 }
@@ -39,9 +33,128 @@ if ( s[i]>='a'&& s[i]<='z')
   }
 return s;
 }
-bool Customer::operator==(const Customer & other)const {
-  return customerID == other.customerID;
+string Customer::getCurrentTimeString(){
+  time_t now = time(0);
+now+=4*3600;
+tm*t = gmtime(&now);
+int hour24 = t->tm_hour;
+int mins = t->tm_min;
+
+string ampm = "AM";
+int hour12 = hour24;
+if(hour24==0)
+  hour12=12;
+else if (hour24==12)
+  ampm = "PM";
+else if (hour24>12){
+hour12=hour24-12;
+ampm="PM";
 }
+string result="";
+if (hour12>=10){
+result+=(char)(hour12/10+'0');
+result+=(char)(hour12%10+'0');
+}
+else {
+result+=(char)(hour12+'0');
+}
+result+=':';
+result+=(char)(mins/10+'0');
+result+=(char)(mins%10+'0');
+result+='';
+result+=ampm;
+
+return result;
+}
+
+Customer::Customer(){
+customerID = 0;
+name="";
+phone="";
+plateNumber="";
+disabledPermit=false;
+}
+int Customer::readInt(string message, int minValue, int maxValue){
+string input;
+int value;
+
+while(true){
+cout<<message;
+getline(cin, input);
+
+if(!isDigitsOnly(input)){
+cout<<"ERROR: Please enter number only.\n";
+continue;
+}
+value = stringToInt(input);
+if(value<minValue||value>maxValue){
+cout<<"ERROR : Number must be between "<<minValue<<"and"<<maxValue<<".\n";
+continue;
+}
+return value;
+}
+}
+int Customer::readExactDigitsInt(string message, int digits){
+  string input;
+while(true){
+cout<<message;
+getline(cin,input);
+
+if(!isDigitsOnly(input)){
+cout<<"ERROR: Please enter numbers only.\n";
+continue;
+}
+if(input.length()!=digits){
+cout<<"ERROR:ID must be exactly"<<digits<<"digits.\n";
+continue;
+}
+return stringToInt(input);
+}
+}
+string Customer::readName(){
+  string name;
+while(true){
+cout<<"Enter name:";
+getline(cin, name);
+
+if(name==""){
+cout<<"ERROR: Name cannot be empty.\n";
+continue;
+}
+string clean = "";
+for(charc:input){
+  if(c!=''){
+  if(c>='a'&& c<='Z')
+  clean+=(char)(c-32);
+  else
+  clean+=c;
+  }
+}
+int colonPos=-1;
+for(int i=0;i<(int)clean.length();i++){
+if(clean[i]==':'){
+colonPos=i;
+break;
+}
+}
+if(colonPos ==-1){
+cout<<'Error:Missing colon.Use H:MM format (e.g. 4:30 PM).\n";
+continue;
+}
+string hourStr=clean.substr(0,colonPos);
+string rest = clean.substr(colonPos +1);
+string minStr = "";
+string ampm = "";
+for(int i =0; i<(int)rest.length(); i++) {
+if(isDigitChar(rest[i]))
+  minStr+= rest[i];
+else{
+ampm = rest.substr(i);
+break;
+}
+}
+
+
 
     
 
