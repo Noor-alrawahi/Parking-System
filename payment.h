53ea14244f3bddc4c9a8db55c24262faec304d57
Mazayin <24-0572@student.gutech.edu.om>
2026-05-25 22:01:02 +0400

    Update payment.h

--- payment.h
+++ payment.h
@@ -1,16 +1,29 @@
-// payment class
+// payment.h
+
 #ifndef PAYMENT_H
 #define PAYMENT_H
 
 #include <iostream>
 using namespace std;
+
+// Payment class 
+// Calculates and displays parking payment.
+
 class Payment{
 private:
+
+// Final payment amount 
   double amount;
+
 public:
+
+// Constructor
   Payment();
 
+// Calculates total payment
   void calculate( double rate, double duration, bool wrongSlot, bool disabledViolation, bool disabledPermit);
+
+// Displays payment amount
   void display() const;
 };
 
