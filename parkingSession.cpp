979c947dbf9210f2910d35a108a984a1345fa824
Mazayin <24-0572@student.gutech.edu.om>
2026-05-25 21:55:24 +0400

    Update parkingSession.cpp

--- parkingSession.cpp
+++ parkingSession.cpp
@@ -1,6 +1,9 @@
-// parkingSession class
+// parkingSession.cpp
 
 #include "ParkingSession.h"
+
+// Constructor
+// Initializes default values
 
 ParkingSession::ParkingSession(){
 
@@ -10,29 +13,55 @@ ParkingSession::ParkingSession(){
    occupied=false;
 }
 
+// startSession()
+// Saves reservation ID and entry time
+
 void ParkingSession::startSession( int resID, double start){
     reservationID=resID;
     startMinutes=start;
+
+   // Vehicle is now parked 
     occupied=true;
 }
 
+// endSession()
+// Saves exit time and free slots
+
 void ParkingSession::endSession(double end){
   endMinutes=end;
+
+   // Vehicle has exited
   occupied=false;
 }
+
+// Returns reservation ID
 
 int ParkingSession::getReservationID() const{
   return reservationID;
 }
 
+// Returns occupancy status
+
 bool ParkingSession::isOccupied() const{
   return occupied;
 }
 
+// getDuration()
+// Calculates parking duration in minutes
+// Handles overnight parking
+// Example:
+// Start= 11:00 PM
+// End= 1:00 AM
+
 double ParkingSession::getDuration() const {
+
+   // Total minutes in one day 
   double minutesInDay=24.0*60.0; 
+
+   //Calculate duration 
   double duration=endMinutes-startMinutes;
 
+   // If negative, add one full day 
   if (duration<=0)
     duration +=  minutesInDay;
   return duration;
