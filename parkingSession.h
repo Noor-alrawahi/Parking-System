fa92661a42b066c5df3a85272e404a8441f6f6b1
Mazayin <24-0572@student.gutech.edu.om>
2026-05-25 21:40:06 +0400

    Update parkingSession.h

--- parkingSession.h
+++ parkingSession.h
@@ -1,7 +1,13 @@
-// parkingSession class
+// parkingSession.h
 
 #ifndef PARKINGSESSION_H
 #define PARKINGSESSION_H
+
+// ParkingSession Class
+// Stores parking session information such as:
+// reservation ID, entry time, exit time,
+// and whether the slot is currently occupied.
+
 class ParkingSession{
  private:
   int reservationID;
@@ -10,12 +16,23 @@ class ParkingSession{
   bool occupied;
 
 public:
+
+// Constructor
    ParkingSession();
 
+// Starts a parking session 
    void startSession( int resID, double start );
+
+// Ends a parking session
    void endSession(double end);
+
+// Returns reservation ID
    int getReservationID() const;
+
+// Returns parking status
    bool isOccupied() const;
+
+// Calculates parking duration
    double getDuration() const;
 };
 
